412d9946e42b01df5be6d680e66602064c032f9d
Rasmus E. Benestad <rasmus.benestad@gmail.com>
2014-11-17 08:51:18 +0100

    Update retrieve.rcm.R

--- R/retrieve.rcm.R
+++ R/retrieve.rcm.R
@@ -10,7 +10,8 @@ retrieve.rcm <- function(ncfile,param=NULL,is=NULL,it=NULL,verbose=TRUE) {
   tunit <- tatt[[itunit]]
   a <- regexpr("since",tunit)
   torg <- substr(tunit,a + attr(a,'match.length')+1,a + attr(a,'match.length')+10)
-  tunit <- tolower(substr(tunit,1,a-2))[1]
+  tunit <- tolower(substr(tunit,1,a-2))
+  browser()
   
   # Extract unit etc for the parameter
   vatt <- ncatt_get( ncold, varid=param )
@@ -47,6 +48,7 @@ retrieve.rcm <- function(ncfile,param=NULL,is=NULL,it=NULL,verbose=TRUE) {
   
   # Extract only the time of interest: assume only an interval
   time <- ncvar_get(ncold,varid='time')
+  print(tunit)
   time <- switch(str(tunit,1,4),'day'=as.Date(time+julian(as.Date(torg))),
        'month'=as.Date(julian(as.Date(paste(time%/%12,time%%12+1,'01',sep='-'))) + julian(as.Date(torg))))
   if (verbose) print(paste(start(time),end(time),sep=' - '))
