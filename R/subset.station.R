6e6db0328cc0b5b20d4d4205a2ea2f3686b646c2
Rasmus E. Benestad <rasmus.benestad@gmail.com>
2014-11-21 12:21:57 +0100

    Update subset.station.R

--- R/subset.station.R
+++ R/subset.station.R
@@ -238,6 +238,7 @@ station.subset <- function(x,it=NULL,is=NULL,verbose=FALSE) {
     # otherwise the subindexing results in an empty object
     
     if (is.logical(is)) is <- (1:length(is))[is]
+    browser()
     y <- x[it,is]
 
     class(x) <- cls; class(y) <- cls
