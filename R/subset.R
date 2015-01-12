abe000019b66e04832808c87ff6aac97eb59b4ff
Rasmus E. Benestad <rasmus.benestad@gmail.com>
2015-01-12 13:47:50 +0100

    Update subset.R
    ```r
    switch(it, `1` = 1, `2` = 4, `3` = 7, `4` = 10)
    ```

--- R/subset.R
+++ R/subset.R
@@ -156,7 +156,7 @@ subset.field <- function(x,it=NULL,is=NULL,verbose=FALSE) {
         } 
         else if (is.element('month',cls) & (max(it) <= 12) ) y <- x[months==it,]
         else if (is.element('season',cls)) {
-            it <- switch(it,'1'=1,'2'=4,'3'=7,'4'=10)
+            it <- switch(it,'1'=1,'2'=4,'3'=7,'4'=10,,'djf'=1,'mam'=4,'jja'=7,'son'=10)
             y <- x[months==it,]
         }
         else if (is.element('annual',cls)) y <- x[years==it,]
