db6712ec5e13121304aa2db9a3f44029c23c813e
abdelkaderm <abdelkaderm@met.no>
2015-08-06 13:46:38 +0200

    Updates in plot.R

--- R/plot.R
+++ R/plot.R
@@ -15,11 +15,11 @@ plot.station <- function(x,plot.type="single",new=TRUE,
   ##     else              
   ##         ylim <- apply(x,2,pretty,n=5)
 
-##<<<<<<< HEAD
-##=======
-##  if (is.null(ylim))
-##      ylim <- pretty(as.numeric(x))
-##>>>>>>> d6d9c84656c9b9b73e711c2e7ee2c8d0fb230980
+  ##<<<<<<< HEAD
+  ##=======
+  if (is.null(ylim))
+      ylim <- pretty(as.numeric(x))
+  ##>>>>>>> d6d9c84656c9b9b73e711c2e7ee2c8d0fb230980
   unit <- attr(x,'unit')[1]
   for (i in 1:length(unit)) {
     if ( (is.na(unit[i]) | is.null(unit[i])) ) unit[i] <- " "
