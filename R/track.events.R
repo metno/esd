37a00875498b4170097d0eda7a67291789d77793
Kajsa Parding <kajsa.parding@met.no>
2015-10-27 12:57:39 +0100

    updated tracking algorithm

--- R/track.events.R
+++ R/track.events.R
@@ -1,37 +1,38 @@
 # K Parding, 15.10.2015
 
 Track.events <- function(x,x0=NULL,it=NULL,is=NULL,dmax=8E4,nmax=31*24,
-                         lplot=FALSE,progress=TRUE,verbose=FALSE) {
+                         nmin=5,dmin=1E5,lplot=FALSE,progress=TRUE,
+                         verbose=FALSE) {
   if (verbose) print("Track.events - nearest neighbour cyclone tracking")
   stopifnot(inherits(x,"events"))
   x <- subset(x,it=!is.na(x["date"][[1]]))
   x <- subset(x,it=it,is=is)
-  yrmn <- as.yearmon(strptime(x["date"][[1]],"%Y%m%d"))
-  if (length(unique(yrmn))>1) {
-    x.tracked <- NULL
-    if (progress) pb <- txtProgressBar(style=3)
-    for (i in 1:length(unique(yrmn))) {
-      if (progress) setTxtProgressBar(pb,i/(length(unique(yrmn))))
-      if(verbose) print(unique(yrmn)[i])
-      x.y <- subset(x,it=(yrmn==unique(yrmn)[i]))
-      x.t <- Track.events(x.y,x.tracked,lplot=lplot,
+  if (!("trajectory" %in% names(x))) {
+    yrmn <- as.yearmon(strptime(x["date"][[1]],"%Y%m%d"))
+    if (length(unique(yrmn))>1) {
+      x.tracked <- NULL
+      if (progress) pb <- txtProgressBar(style=3)
+      for (i in 1:length(unique(yrmn))) {
+        if (progress) setTxtProgressBar(pb,i/(length(unique(yrmn))))
+        if(verbose) print(unique(yrmn)[i])
+        x.y <- subset(x,it=(yrmn==unique(yrmn)[i]))
+        x.t <- Track.events(x.y,x.tracked,lplot=lplot,
                           progress=FALSE,verbose=verbose)
-      if (is.null(x.tracked)) {
-        x.tracked <- x.t
-      } else {
-        x.tracked <- merge(x.tracked,x.t,all=TRUE)
+        if (is.null(x.tracked)) {
+          x.tracked <- x.t
+        } else {
+          x.tracked <- merge(x.tracked,x.t,all=TRUE)
+        }
       }
-    }
-    x <- x.tracked
-    x <- attrcp(x.t,x)
-    class(x) <- class(x.t)
-    invisible(x)
-  } 
-  dates <- x["date"][[1]]
-  times <- x["time"][[1]]
-  lons <- x["lon"][[1]]
-  lats <- x["lat"][[1]]
-  if (!("tracknumber" %in% names(x))) {
+      x <- x.tracked
+      x <- attrcp(x.t,x)
+      class(x) <- class(x.t)
+      invisible(x)
+    } 
+    dates <- x["date"][[1]]
+    times <- x["time"][[1]]
+    lons <- x["lon"][[1]]
+    lats <- x["lat"][[1]]
     num <- rep(NA,dim(x)[1])
     dx <- rep(NA,dim(x)[1])
     datetime <- strptime(paste(dates,times),"%Y%m%d %H")
@@ -39,14 +40,14 @@ Track.events <- function(x,x0=NULL,it=NULL,is=NULL,dmax=8E4,nmax=31*24,
     dh <- mean(as.numeric(diff(d,units="hours")))
     dmax <- dmax*dh
     nmax <- nmax/dh
-    if (all(c("date","time","lon","lat","tracknumber") %in% names(x0))) {
+    if (all(c("date","time","lon","lat","trajectory") %in% names(x0))) {
       dt0 <- strptime(paste(x0["date"][[1]],x0["time"][[1]]),"%Y%m%d %H")
       x0 <- x0[dt0==max(dt0[dt0<min(datetime)]),]
       dates <- c(x0["date"][[1]],dates)
       times <- c(x0["time"][[1]],times)
       lons <- c(x0["lon"][[1]],lons)
       lats <- c(x0["lat"][[1]],lats)
-      num <- c(x0["tracknumber"][[1]],num)
+      num <- c(x0["trajectory"][[1]],num)
       dx <- c(rep(0,dim(x0)[1]),dx)
       datetime <- strptime(paste(dates,times),"%Y%m%d %H")
       i.start <- length(x0["lon"][[1]])+1
@@ -70,7 +71,7 @@ Track.events <- function(x,x0=NULL,it=NULL,is=NULL,dmax=8E4,nmax=31*24,
     if (verbose) print(paste('Nearest neighbour tracking took',
                 round(as.numeric(t2-t1,units="secs")),'s'))
   } else {
-    num <- x["tracknumber"][[1]]
+    num <- x["trajectory"][[1]]
     dx <- x["distance"][[1]]
     i.start <- 1
   }
@@ -88,7 +89,25 @@ Track.events <- function(x,x0=NULL,it=NULL,is=NULL,dmax=8E4,nmax=31*24,
       tracklen <- data.frame(table(num))
     }
   }
+  dx[is.na(dx)] <- 0
+  x$trajectory <- num[i.start:length(num)]
+  x$distance <- dx[i.start:length(num)]
+  if ( length(names(x))>length(attr(x,"unit")) ) {
+    attr(x,"unit") <- c(attr(x,"unit"),c("numeration","km"))
+  }
+  if (verbose) print("calculate trajectory statistics")
+  x <- Trackstats(x)
+  n <- x["trackcount"][[1]]
+  options(digits=12)
+  dnum <- as.numeric(paste(x["date"][[1]],x["time"][[1]],sep="."))
+  y <- subset(x,it=(n>nmin & dnum!=min(dnum) & dnum!=max(dnum)))
+  rnum <- Enumerate(y)
+  if (i.start>1) rnum <- rnum + max(num[1:(i.start-1)])
+  y["trajectory"] <- rnum
   if(lplot) {
+    lons <- y["lon"][[1]]
+    lats <- y["lat"][[1]]
+    num <- y["trajectory"][[1]]
     data(geoborders,envir=environment())
     plot(geoborders,type="l",col="grey20",lwd=0.5,xlim=c(-180,180),ylim=c(0,90))
     for (i in tracklen$num[tracklen$Freq>q995(tracklen$Freq)]) {
@@ -97,9 +116,7 @@ Track.events <- function(x,x0=NULL,it=NULL,is=NULL,dmax=8E4,nmax=31*24,
       lines(lons[num==i],lats[num==i],col="blue",lty=1,lwd=1)
     }
   }
-  dx[is.na(dx)] <- 0
-  x$tracknumber <- num[i.start:length(num)]
-  x$distance <- dx[i.start:length(num)]
+  
   invisible(x)
 }
 
@@ -125,6 +142,7 @@ NearestNeighbour <- function(lon1,lat1,lon2,lat2,dmax=1E6,lplot=FALSE,
     }
   }
   d.num[is.na(num)] <- 0
+  d.num <- d.num*1E-3
   if (lplot) {
     plot(lon1,lat1,type="p",col="black",pch=1:length(lon1),lwd=1,
        xlim=c(-180,180),ylim=c(0,90))
@@ -133,26 +151,53 @@ NearestNeighbour <- function(lon1,lat1,lon2,lat2,dmax=1E6,lplot=FALSE,
   invisible(cbind(num,d.num))
 }
 
-
-Trackstats.events <- function(x,verbose=FALSE) {
-  if(verbose) print("Trackstats.events")
+Trackstats <- function(x,verbose=FALSE) {
+  if(verbose) print("Trackstats")
   stopifnot(inherits(x,"events"))
-  if (!any("tracknumber" %in% names(x))) x <- Track.events(x)
-  lons <- x["lon"][[1]]
-  lats <- x["lat"][[1]]
-  num <- x["tracknumber"][[1]]
-  dx <- x["distance"][[1]]
-  dx[is.na(dx)] <- 0
+  if (!any("trajectory" %in% names(x))) x <- Track.events(x,verbose=verbose)
+  y <- x[order(x["trajectory"][[1]]),]
+  y <- attrcp(x,y)
+  class(y) <- class(x)
+  lons <- y["lon"][[1]]
+  lats <- y["lat"][[1]]
+  rnum <- Enumerate(y)
+  #num <- y["trajectory"][[1]]
+  #rnum <- num[2:length(num)]-num[1:(length(num)-1)]
+  #rnum[rnum>0] <- 2:(sum(rnum>0)+1)
+  #rnum <- c(1,rnum)    
+  #while(any(rnum==0)) {
+  #  rnum[rnum==0] <- rnum[which(rnum==0)-1]
+  #}
+  dy <- y["distance"][[1]]
+  dy[is.na(dy)] <- 0
   if(verbose) print("trackcount")
-  trackcount <- data.frame(table(num))
-  x$trackcount <-  trackcount$Freq[num]
+  trackcount <- data.frame(table(rnum))
   if(verbose) print("timestep")
-  ts <- unlist(sapply(unique(num),function(i) 1:trackcount$Freq[i]))
+  ts <- unlist(sapply(unique(rnum),function(i) 1:trackcount$Freq[i]))
   timestep <- rep(NA,length(ts))
-  timestep[order(num)] <- ts
-  x$timestep <- timestep
+  timestep[order(rnum)] <- ts
   if(verbose) print("tracklength")
-  tracklength <- as.numeric(by(dx,num,sum))
-  x$tracklength <- tracklength[num]
-  invisible(x)
+  #tracklength <- as.numeric(by(dy,rnum,sum))
+  #lon1 <- y[]
+  y$trackcount <-  trackcount$Freq[rnum]
+  y$timestep <- timestep
+  y$tracklength <- tracklength[rnum]
+  if (length(attr(y,"unit"))<dim(y)[2]) {
+    attr(y,"unit") <- c(attr(y,"unit"),c("count","numeration","km"))
+  }
+  invisible(y)
+}
+
+Enumerate <- function(x,verbose=TRUE) {
+  if(verbose) print("Enumerate")
+  stopifnot(inherits(x,"events"))
+  if (!any("trajectory" %in% names(x))) x <- Track.events(x,verbose=verbose)
+  num <- x["trajectory"][[1]]
+  rnum <- num[2:length(num)]-num[1:(length(num)-1)]
+  rnum[rnum>0] <- 2:(sum(rnum>0)+1)
+  rnum <- c(1,rnum)    
+  while(any(rnum==0)) {
+    rnum[rnum==0] <- rnum[which(rnum==0)-1]
+  }
+  invisible(rnum)
 }
