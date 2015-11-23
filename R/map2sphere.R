2fc402140592a2d64ffa7d4c0085645eda844ad5
brasmus <rasmus.benestad@met.no>
2015-11-23 11:03:43 +0100

    allow for no variable label in map2sphere

--- R/map2sphere.R
+++ R/map2sphere.R
@@ -116,8 +116,10 @@ map2sphere <- function(x,it=NULL,is=NULL,new=TRUE,
   param <- attr(x,'variable')
   unit <- attr(x,'unit')
   ## KMP 10-11-2015: prepare unit and parameter labels
-  if(!is.null(param) & !inherits(param,'expression')) param <- gsub(" ","~",param)
-  if(!is.null(unit) & !inherits(param,'expression')) unit <- gsub(" ","~",unit)
+  if(!is.null(param) & !inherits(param,'expression'))
+    param <- gsub(" ","~",param)
+  if(!is.null(unit) & !inherits(param,'expression'))
+    unit <- gsub(" ","~",unit)
   if(length(param)>1) param <- param[1]
   if(length(unit)>1) unit <- unit[1]
   if (is.T(x)) {
@@ -288,12 +290,13 @@ map2sphere <- function(x,it=NULL,is=NULL,new=TRUE,
   ## plot(range(x,na.rm=TRUE),range(z,na.rm=TRUE),type="n",
   ##     xlab="",ylab="",add=FALSE)
   txt <- param
-  param <- as.character(param); unit <- as.character(unit)
-  if(!is.null(unit) & (unit!='')) txt <- paste(param,'~(',unit,')') else
-     if(!is.null(unit)) txt <- param
-  text(min(x),max(z),eval(parse(text=paste('expression(',txt,')'))),
+  if (!is.null(param)) {
+    param <- as.character(param); unit <- as.character(unit)
+    if(!is.null(unit) & (unit!='')) txt <- paste(param,'~(',unit,')') else
+      if(!is.null(unit)) txt <- param
+    text(min(x),max(z),eval(parse(text=paste('expression(',txt,')'))),
        cex=1.5,pos=4) 
-  
+  }
   #result <- data.frame(x=colMeans(Y),y=colMeans(Z),z=c(map))
   result <- NULL # For now...
   invisible(result)
