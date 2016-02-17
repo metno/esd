4e2b52585df0024fcbfe4678cf125a779513bf74
Kajsa Parding <kajsa.parding@met.no>
2016-02-17 14:52:04 +0100

    updated plot.dsensemble

--- R/plot.R
+++ R/plot.R
@@ -1203,8 +1203,12 @@ plot.dsensemble <-  function(x,pts=FALSE,it=0,
   #browser()
   index(y) <- year(y)
   #print("---")
-
-  pscl <- c(0.9,1.1)
+  
+  if(map.show | target.show) {
+    pscl <- c(0.9,1.4)
+  } else {
+    pscl <- c(0.9,1.1)
+  }
   if (max(coredata(z),na.rm=TRUE) < 0) pscl <- rev(pscl)
   args <- list(...)
   #print(names(args))
