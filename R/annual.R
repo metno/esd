1e50041eeb3336310ebc280cfbf94af73c072784
brasmus <rasmus.benestad@met.no>
2016-03-07 14:57:42 +0100

    improved pentad - added argument it0

--- R/annual.R
+++ R/annual.R
@@ -464,7 +464,8 @@ season.abb <- function() {
 pentad <- function(x,l=5,it0=NULL,...) {
   if (!is.null(it0)) yr <- year(x) - it0 else yr <- year(x)
   yrl <- l*trunc(yr/l)
-  if (!is.null(it0)) index(x) <- yrl + it0 else index(x) <- yrl
+  if (!is.null(it0)) yrl <- yrl + it0  
+  index(x) <- yrl
   
   xl <- aggregate(x,yrl,...)
   attr(xl,'dimnames') <- NULL
