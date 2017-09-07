21ba0f0c812b67e55aff6acddffbb724d89af50f
kajsamp <kajsamp@met.no>
2017-09-07 10:19:05 +0200

    minor updates map.station

--- R/map.station.R
+++ R/map.station.R
@@ -327,13 +327,8 @@ map.station.old <- function (x=NULL,FUN=NULL, it=NULL,is=NULL,new=FALSE,
     
     tte <- "rwb"
     
-    ##<<<<<<< HEAD
-    ##if (is.null(cex))
-    ##    cex <- 5/log(dim(ss)[1])
-    ##=======
     ## An attempt to set the size of the symbols automatically,
     ## but this fails if ss is a list.
-    
     nok <- apply(coredata(x),2,FUN='nv')
     if (verbose) { print('nok:'); print(nok) }
     if ( (is.null(cex)) & !is.null(dim(ss)) )
@@ -341,10 +336,8 @@ map.station.old <- function (x=NULL,FUN=NULL, it=NULL,is=NULL,new=FALSE,
         if (is.null(cex)) cex <- 5/log(length(ss[[1]])) else
           if (cex==0) cex <- 1.25*nok/max(nok,na.rm=TRUE)
     if (cex<0) cex <- abs(cex)*nok/max(nok,na.rm=TRUE)
-    ##>>>>>>> d6d9c84656c9b9b73e711c2e7ee2c8d0fb230980
-    
+
     ## Select a subdomain in the x-axis
-    
     if (verbose) { print('cex:'); print(cex) }
     if (is.null(xlim))
       if (is.null(highlight) | showall)
