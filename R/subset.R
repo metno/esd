cb23885f7e8a808faa4a7a84cae00b9e84d94a22
kajsamp <kajsamp@met.no>
2017-09-20 09:59:46 +0200

    updated subset.trajectory and subset.events

--- R/subset.R
+++ R/subset.R
@@ -1091,12 +1091,13 @@ default.subset <- function(x,it=NULL,is=NULL,verbose=FALSE) {
 }
     
 
-subset.events <- function(x,it=NULL,is=NULL,verbose=FALSE,...) {
+subset.events <- function(x,it=NULL,is=NULL,ic=NULL,verbose=FALSE,...) {
   if(verbose) print("subset.events")
   cls <- class(x)
   
   if (length(it)==0) it <- NULL
   if (length(is)==0) is <- NULL
+  if (length(ic)==0) ic <- NULL
   ii <- rep(TRUE,dim(x)[1])
   
   if(!is.null(it)) {
@@ -1200,13 +1201,205 @@ subset.events <- function(x,it=NULL,is=NULL,verbose=FALSE,...) {
     warning("default.subset: did not reckognise the selection citerion for 'is'")
   }
   
-  ij <- ii & jj
-  y <- x[ij,]
+  kk <- rep(TRUE,dim(x)[1])
+  if(is.list(ic)) {
+    if(!is.null(ic$param) & (!is.null(ic$pmin) | !is.null(ic$pmax))) {
+      if(ic$param %in% c("trajectory","trackcount","distance","tracklength")) {
+        ic$FUN <- NULL
+        if(!ic$param %in% names(x)) x <- Trackstats(x)
+      } else if(is.null(ic$FUN)) {
+        ic$FUN <- "any"
+      }
+      if(!is.null(ic$FUN)) {
+        if(!ic$FUN=="all" & !"trajectory" %in% names(x)) x <- track(x)
+      }
+      if(!ic$param %in% names(x)) {
+        if(verbose) print(paste("Unkown input param =",param))
+      } else {
+        if(is.null(ic$pmin)) ic$pmin <- min(x[ic$param],na.rm=TRUE)
+        if(is.null(ic$pmax)) ic$pmax <- max(x[ic$param],na.rm=TRUE)
+        if(verbose) print(paste(ic$param,"in range",ic$pmin,"-",ic$pmax))
+        if(verbose) print(paste("FUN =",ic$FUN))
+        if(is.null(ic$FUN)) {
+          kk <- as.vector(x[ic$param]>=ic$pmin & x[ic$param]<=ic$pmax)
+        } else if (ic$FUN=="any") {
+          ok.ev <- as.vector(x[ic$param]>=ic$pmin & x[ic$param]<=ic$pmax)
+          kk <- x$trajectory %in% unique(x$trajectory[ok.ev])
+        } else if (ic$FUN=="all") {
+          nok.ev <- as.vector(x[ic$param]<ic$pmin | x[ic$param]>ic$pmax)
+          kk <- !x$trajectory %in% unique(x$trajectory[nok.ev])
+        }
+      }
+    }
+  }
+  
+  ijk <- ii & jj & kk
+  y <- x[ijk,]
   attr(y,"aspect") <- "subset"
   attr(y,'history') <- history.stamp(x)  
   if (!is.null(is$lat)) attr(y,"lat") <- is$lat
   if (!is.null(is$lon)) attr(y,"lon") <- is$lon
   class(y) <- cls
+  invisible(y)
+}
+
+subset.trajectory <- function(x,it=NULL,is=NULL,ic=NULL,verbose=FALSE) {
+  if(verbose) print("subset.trajectory")
+  
+  x0 <- x
+  cls <- class(x)
+  if (is.null(it) & is.null(is) & is.null(ic)) return(x)
+  
+  l <- dim(x)[1]
+  if (is.null(it)) ii <- rep(TRUE,l)
+  if (is.null(is)) ij <- rep(TRUE,l)
+  if (is.null(ic)) ik <- rep(TRUE,l)
+  
+  # Generate sequence of days, months or years if range of it value is given
+  if (!is.null(it)) {
+    if(verbose) print('Generate sequence of time if it value is given')
+    tstart <- strftime(strptime(x[,colnames(x)=="start"],format="%Y%m%d%H"),
+                       format="%Y-%m-%d")
+    tend <- strftime(strptime(x[,colnames(x)=="end"],format="%Y%m%d%H"),
+                     format="%Y-%m-%d")
+    yr <- year(x)
+    mo <- month(x)
+    dy <- day(x)
+    if(verbose) print(paste('length of t',length(t),'yr',length(yr),
+                            'mo',length(mo),'dy',length(dy)))
+    if(verbose) print(paste('years',paste(unique(yr),collapse=",")))
+    if(verbose) print(paste('months',paste(unique(mo),collapse=",")))
+    if(verbose) print(paste('mdays',paste(unique(dy),collapse=",")))
+    
+    is.months <- function(x) all(sum(is.element(tolower(substr(x,1,3)),
+                                                tolower(month.abb)))>0)
+    is.seasons <- function(x) all(sum(is.element(tolower(substr(x,1,3)),
+                                                 names(season.abb())))>0)
+    is.dates <- function(x) all(!is.months(x) &
+                                  (levels(factor(nchar(x)))==10) |
+                                  (is.numeric(x) & levels(factor(nchar(x)))==8))
+    is.years <- function(x) all(!is.months(x) & 
+                                  is.numeric(x) & levels(factor(nchar(x)))==4)
+    
+    if (is.months(it)) {
+      if (verbose) print('Monthly selected')
+      ii <- is.element(mo,(1:12)[is.element(tolower(month.abb),
+                                            tolower(substr(it,1,3)))])
+    } else if (is.seasons(it)) {
+      if (verbose) print("Seasonally selected")
+      if (verbose) print(table(mo))
+      if (verbose) print(eval(parse(text=paste('season.abb()$',it,sep=''))))
+      ii <- is.element(mo,eval(parse(text=paste('season.abb()$',it,sep=''))))
+    } else if (is.dates(it)) {
+      it <- strftime(as.Date(it),format="%Y%m%d")
+      t1 <- strftime(as.Date(tstart),format="%Y%m%d")
+      t2 <- strftime(as.Date(tend),format="%Y%m%d")
+      if ( length(it) == 2 ) {
+        if (verbose) print('Between two dates')
+        if (verbose) print(it)
+        ii <- t1>min(it) & t1<max(it) | t2>min(it) & t2<max(it)
+        #it <- strftime(seq(it[1],it[2],by='day'),format="%Y%m%d")
+        #t <- strftime(t,format="%Y%m%d")       
+        #ii <- is.element(t,it)
+      } else { 
+        if (verbose) print('it is a string of dates')
+        ii <- it<t2 & it>t1
+        #ii <- is.element(t,it)
+      }
+    } else if (is.years(it)) {
+      it <- as.integer(it)
+      if ( length(it) == 2 ) {
+        if (verbose) print('Between two years')
+        if (verbose) print(it)          
+        ii <- is.element(yr,seq(it[1],it[2],1))
+      } else { 
+        if (verbose) print('it is a string of years')
+        ii <- is.element(yr,it)
+      }
+    } else if (is.logical(it) & length(it)==length(yr)) {
+      if (verbose) print('it is a logical array')
+      ii <- it
+    } else if (is.numeric(it) & max(it)<=length(yr)) {
+      if (verbose) print('it is an index array')
+      ii <- rep(FALSE,length(yr))
+      ii[it] <- TRUE
+    } else {
+      ii <- rep(FALSE,length(yr))
+      warning("subset.trajectory: did not recognise the selection citerion for 'it'")
+    }
+  }
+  
+  # is can be a list to select region or according to other criterion
+  if (inherits(is,'list')) {
+    selx <- rep(TRUE,l); sely <- selx;
+    selp <- selx; selF <- selx
+    nms <- names(is)
+    ix <- grep('lon',tolower(nms))
+    iy <- grep('lat',tolower(nms))
+    ip <- grep('slp',tolower(nms))
+    iF <- grep('FUN',nms)
+    if (length(ix)>0) slon <- is[[ix]] else slon <- NULL
+    if (length(iy)>0) slat <- is[[iy]] else slat <- NULL
+    if (length(ip)>0) sslp <- is[[ip]] else sslp <- NULL        
+    if (length(iF)>0) sFUN <- is[[iF]] else sFUN <- NULL
+    if (length(slon)==2 & length(slat)==2) {
+      if (verbose) print(paste('is selects longitudes ',
+                               slon[1],'–',slon[2],'E ',
+                               "and latitudes ",
+                               slat[1],'–',slat[2],'N ',sep=""))
+      jx <- colnames(x)=='lon'
+      jy <- colnames(x)=='lat'
+      selx <- apply(x,1,function(x) any(x[jx]>=min(slon) &
+                                          x[jx]<=max(slon) & x[jy]>=min(slat) & x[jy]<=max(slat)))        
+    } else if (length(slon)==2) {
+      if (verbose) print(paste('is selects longitudes ',
+                               slon[1],'–',slon[2],'E',sep=""))
+      jx <- colnames(x)=='lon'
+      selx <- apply(x,1,function(x) any(x[jx]>=min(slon) & x[jx]<=max(slon)))
+    } else if (length(slat)==2) {
+      if (verbose) print(paste('is selects latitudes ',
+                               slat[1],'–',slat[2],'N',sep=""))
+      jy <- colnames(x)=='lat'
+      selx <- apply(x,1,function(x) any(x[jy]>=min(slat) & x[jy]<=max(slat)))
+    }
+    ij <- selx & selp & selF
+  }
+  
+  if(is.list(ic)) {
+    if(is.null(ic$FUN)) ic$FUN <- "any"
+    if(!is.null(ic$param) & (!is.null(ic$pmin) | !is.null(ic$pmax))) {
+      if(!ic$param %in% colnames(x)) {
+        if(verbose) print(paste("Unkown input ic$param =",ic$param))
+      } else {
+        ip <- colnames(x)==ic$param
+        if(is.null(ic$pmin)) ic$pmin <- min(x[,ip],na.rm=TRUE)
+        if(is.null(ic$pmax)) ic$pmax <- max(x[,ip],na.rm=TRUE)
+        if(verbose) print(paste(ic$param,"in range",ic$pmin,"-",ic$pmax))
+        if(verbose) print(paste("FUN =",ic$FUN))
+        if (ic$FUN %in% c("any","all")) {
+          fn <- function(x) do.call(ic$FUN,list(x[ip]>=ic$pmin & x[ip]<=ic$pmax))
+        } else {
+          fn <- function(x) do.call(ic$FUN,list(x[ip]))>=ic$pmin &  do.call(ic$FUN,list(x[ip]))<=ic$pmax
+        }
+        ik <- apply(x,1,fn)
+      }
+    }
+  }
+  
+  if(verbose) print(paste('length(ii)',length(ii),'length(ij)',length(ij),'length(ik)',length(ik)))
+  if(verbose) print(paste('it selects',sum(ii),'is selects',sum(ij),'ic selects',sum(ik)))
+  ist <- (1:l)[(ii & ij & ik)]
+  y <- x[ist,]
+  if(verbose) print(paste('total subset',sum(ii & ij & ik)))
+
+  class(y) <- cls
+  y <- attrcp(x,y)
+  if (inherits(is,'list')) {
+    if (length(slon)==2) attr(y,'longitude') <- slon
+    if (length(slat)==2) attr(y,'latitude') <- slat
+  }
+  if (is.seasons(it) | is.months(it)) class(y) <- c(class(y),'season')
+  attr(y,'history') <- history.stamp(x)
   invisible(y)
 }
 
