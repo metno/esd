8ca2e9c7af2a4845bbd5e1f5b2fa5a3137f99f67
kajsamp <kajsamp@met.no>
2018-11-08 20:59:58 +0100

    added man file for cyclonebudget functions

--- R/gdcn.R
+++
@@ -1,8 +0,0 @@
-gdcn <- function(param="t2m",cntr=NULL) {
-
-# KMP 2018-11-02: I don't think this file is supposed to be in esd/R
-# Neihther of these data files exist in esd/data 
-#data("gdcn.inv", envir = environment())
-#load("mapgdcn.rda")
-
-}
