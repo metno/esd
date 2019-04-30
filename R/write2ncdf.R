72c69e0de09b11c902d8e93327076fa65d60e814
Rasmus Benestad <rasmusb@pc5055.pc.met.no>
2019-04-30 13:03:11 +0200

    bug-fix in write2ncdf4.station

--- R/write2ncdf.R
+++ R/write2ncdf.R
@@ -686,7 +686,7 @@ write2ncdf4.station <- function(x,fname,prec='short',offset=0, missval=-99,it=NU
       tsigma2id.jja <- ncid$var[["summary_trend_sigma2_JJA"]]
       tsigma2id.son <- ncid$var[["summary_trend_sigma2_SON"]]
       mwslid <- ncid$var[["summary_mean_wetdur"]]
-      mwdlid <- ncid$var[["summary_mean_drydur"]]
+      mdslid <- ncid$var[["summary_mean_drydur"]]
     } else {
       sdid <- ncid$var[["summary_sd"]]
       sdid.djf <- ncid$var[["summary_sd_DJF"]]
