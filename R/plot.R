b9fd206a00f4439bf032be07d7e5d578d00f038a
kajsamp <kajsamp@met.no>
2019-05-06 08:00:47 +0200

    moved plot.trajectory to plot.R

--- R/plot.R
+++ R/plot.R
@@ -1695,6 +1695,38 @@ plot.nevents <- function(x,verbose=FALSE,main=NULL,xlab=NULL,ylab=NULL,col=NULL,
   lines(attr(x,'nwd.pre'),col=rgb(0.5,0.5,0.5,0.5))
 }
 
+plot.trajectory <- function(x,it=NULL,is=NULL,
+                            main=NULL,xlim=NULL,ylim=NULL,
+                            col=NULL,pch=0,type='l',lwd=3,
+                            xlab="",ylab=NULL,new=TRUE,verbose=FALSE,...) {
+  if(verbose) print("plot.trajectory")
+  y <- subset(x,it=it,is=is)
+  n <- count.trajectory(y,by='year')
+  if(new) dev.new()
+  plot.station(n,main=main,new=new,col=col,
+               xlim=xlim,ylim=ylim,type=type,
+               lwd=lwd,pch=pch,xlab=xlab,ylab=ylab,
+               legend.show=FALSE,...)
+  if (is.null(col)) col <- rainbow(length(y[1,]))
+  if (!is.null(attr(y,"longitude")) |
+      !is.null(attr(y,"latitude"))) {
+    if(verbose) print("adding legend")
+    par(xpd=TRUE)
+    leg <- ""
+    if (!any(is.na(attr(y,"longitude"))) & !is.null(attr(y,"longitude"))) {
+      leg <- paste(leg,paste(round(range(attr(y,"longitude")),2),sep="-"),"E/",sep="")
+    }
+    if (!any(is.na(attr(y,"latitude"))) & !is.null(attr(y,"latitude"))) {
+      leg <- paste(leg,paste(round(range(attr(y,"latitude")),2),sep="-"),"N",sep="")
+    }
+    if (is.null(col)) col <- rainbow(1)
+    if(verbose) print(paste('legend:',leg,', color:',col[1]))
+    legend("bottomleft",inset=c(0,-0.25),legend=leg,bty="n",cex=0.6,ncol=3,
+           text.col="grey40",lty=1,col=col)
+  }
+  invisible(n)
+}
+
 barplot.station <- function(x,threshold=0,...) {
     stopifnot(inherits(x,'station'))
     x.above <- x.below <- x
