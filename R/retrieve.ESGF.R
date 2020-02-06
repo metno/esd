795a1a220e7b517c3b5afd871607d7fdb403e1d0
Rasmus Benestad <rasmusb@pc5055.pc.met.no>
2020-02-06 09:56:51 +0100

    fixes to retrieve.ESGF

--- R/retrieve.ESGF.R
+++ R/retrieve.ESGF.R
@@ -1,4 +1,5 @@
 #' Retrieve CMIP data directly from the Earth System Grid Federation (ESGF)
+#' https://earthsystemcog.org/projects/cog/esgf_search_restful_api
 
 #' @aliases retrieve.ESGF
 
@@ -24,18 +25,18 @@
 #' processing.
 #'
 #' @examples 
-#' retieve.ESGF(lon=c(-30,40),lat=c(50,70),period='205001-210012') -> X
+#' retrieve.ESGF(lon=c(-30,40),lat=c(50,70),period='205001-210012') -> X
 #' map(X)
 
 #' @export retrieve.ESGF
-
-retieve.ESGF <- function(param='tas',verbose=FALSE,...,
-             url='https://esgf-data3.ceda.ac.uk/thredds/dodsC',CMIP='esg_cmip6/CMIP6',
-             MIP='ScenarioMIP',inst='MOHC',model='UKESM1-0-LL',scen='ssp585',
-             ID='r2i1p1f2',type='Amon',grid='gn',version='v20190507',period='2015-2100') {
+retrieve.ESGF <- function(param='tas',verbose=FALSE,...,
+                          url='https://esgf-data3.ceda.ac.uk/thredds/dodsC',CMIP='esg_cmip6/CMIP6',
+                          MIP='ScenarioMIP',inst='MOHC',model='UKESM1-0-LL',scen='ssp585',
+                          ID='r2i1p1f2',type='Amon',grid='gn',version='v20190507',period='2015-2100') {
   fname <- paste0(paste(param,type,model,scen,ID,grid,period,sep='_'),'.nc')
   opendap <- paste(url,CMIP,MIP,inst,model,scen,ID,type,param,grid,version,fname,sep='/')
   if (verbose) print(opendap)
   x <- try(retrieve(ncfile=opendap,verbose=verbose,...))
-    invisible(x)
+  invisible(x)
+  
 }
