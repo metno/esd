aa94d51a632665f97950804ad46c0019fdf316d9
Rasmus Benestad <rasmusb@pc5055.pc.met.no>
2020-03-30 15:33:14 +0200

    improved meta.ESGF

--- R/retrieve.ESGF.R
+++ R/retrieve.ESGF.R
@@ -13,22 +13,16 @@
 #' @param verbose Logical value defaulting to FALSE. If FALSE, do not display
 #' comments (silent mode). If TRUE, displays extra information on progress.
 #' @param url The base URL for ESGF
-#' @param CMIP Which generation of CMIP
-#' @param MIP Name of activity
-#' @param inst Institution ID
-#' @param model Name of climate model
-#' @param scen Name of scenario
-#' @param ID The name of the run
-#' @param type The table ID
-#' @param grid Grid ID
-#' @param version Version
-#' @param period The time segment of the file
-#' @return A "zoo" "field" object with additional attributes used for further
-#' processing.
+#' @param n Number of models to read - NULL reads everything
+#' @param expid Name of the MIP experiment ('historical' for historical run)
+#' @param mip CMIP5 or CMIP6
+#' @param freq Frequency of data 
+#' @return A data.frame for \code{meta.ESGF} and a "zoo" "field" object with additional attributes used for further
+#' processing for +code{retrieve.ESGF}.
 #'
 #' @examples 
 #' \dontrun{
-#' meta <- meta.ESGF()
+#' meta <- meta.ESGF(n=3)
 #' X <- retrieve.ESGF(im=3,lon=c(-30,40),lat=c(50,70),meta=meta)
 #' map(X)
 #'}
@@ -43,7 +37,7 @@ retrieve.ESGF <- function(im=1,meta=NULL,verbose=FALSE,...) {
 
 #' @export meta.ESGF
 meta.ESGF <- function(url="https://esgf-data.dkrz.de/esg-search/search/",mip="CMIP6",param="tas",
-                      freq="mon",expid="ssp585",verbose=FALSE) {
+                      freq="mon",expid="ssp585",verbose=FALSE,n=NULL) {
   if (verbose) print('meta.ESFG - this function uses the rjson-package to read metadata from ESGF')
   ## Check if the JSON library is installed
   ## Check if you need to get the devtools-package:
@@ -62,6 +56,7 @@ meta.ESGF <- function(url="https://esgf-data.dkrz.de/esg-search/search/",mip="CM
   if (verbose) print(URL)
 
   nof_datasets <- fromJSON(file=URL)$response$numFound
+  if (!is.null(n)) nof_datasets <- n
   print(paste('Found',nof_datasets,'datasets'))
   if (nof_datasets==0) {
     print('The connection seems to be down - try again later')
@@ -86,7 +81,6 @@ meta.ESGF <- function(url="https://esgf-data.dkrz.de/esg-search/search/",mip="CM
       print(paste(rep("=",nchar(ESGF_query$response$docs[[i]]$id)+9),collapse=""))
     } else cat('.')
     
-    
     for (j in 1:nof_files) {
       ic <- as.character(i); if (i < 100) ic <- paste('0',ic,sep=''); if (i < 10) ic <- paste('0',ic,sep='')
       jc <- as.character(j); if (j < 100) jc <- paste('0',jc,sep=''); if (j < 10) jc <- paste('0',jc,sep='')
@@ -97,10 +91,11 @@ meta.ESGF <- function(url="https://esgf-data.dkrz.de/esg-search/search/",mip="CM
       url <- strsplit(ESGF_file_query$response$docs[[j]]$url[1],"|",fixed=TRUE)[[1]][1]
       results[[paste('OpenDAP',ic,jc,sep='_')]] <- gsub("fileServer","dodsC",url)
       #print(paste('OpenDAP',ic,jc,sep='_'))
-      results[[paste('member_id',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[1]]$member_id
-      results[[paste('grid',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[1]]$grid
-      results[[paste('source_id',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[1]]$source_id
-      results[[paste('type',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[1]]$source_type
+      results[[paste('member_id',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[j]]$member_id
+      results[[paste('grid',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[j]]$grid
+      results[[paste('source_id',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[j]]$source_id
+      results[[paste('type',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[j]]$source_type
+      results[[paste('title',ic,jc,sep='_')]] <- ESGF_file_query$response$docs[[j]]$title
     }
   }
   elements <- names(results)
@@ -110,9 +105,13 @@ meta.ESGF <- function(url="https://esgf-data.dkrz.de/esg-search/search/",mip="CM
   grid <- grep('grid',names(results))
   model <- grep('source_id',names(results))
   type <- grep('type',names(results))
+  title <- grep('title',names(results))
+  period <- substr(as.character(results[title]),nchar(as.character(results[title]))-15,
+                   nchar(as.character(results[title]))-3)
   meta <- data.frame(OpenDap=as.character(results[opendap]),http=as.character(results[http]),
                      member.id=as.character(results[mem]),grid=as.character(results[grid]),
-                     model=as.character(results[model]),type=as.character(results[type]))
+                     model=as.character(results[model]),type=as.character(results[type]),
+                     title=as.character(results[title]),period=as.character(period))
   attr(meta,'variable') <- param
   attr(meta,'all.query.data') <- results[grep('file.query',names(results))]
   attr(meta,'history') <- history.stamp(meta)
