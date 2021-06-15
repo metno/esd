3c66eeb46ef843cb7d141a286831c85194da6d4c
Rasmus Benestad <rasmusb@pc5055.pc.met.no>
2021-06-15 07:23:14 +0200

    renamed new functions GHCND.meta and GHCND.station

--- R/GHCND.R
+++ R/GHCND.R
@@ -1,12 +1,12 @@
 #' Fetch station data from GHND and convert into \code{station} objects.
 #' 
 #' Transform an input object into the esd class \code{station}. 
-#' \code{ghcnd.meta} reads the inventory and metadata of the stations (slow due to big data volume - requires Internet access).
+#' \code{GHCND.meta} reads the inventory and metadata of the stations (slow due to big data volume - requires Internet access).
 #'
-#' \code{ghcnd.station} reads the raw data (slow due to big data volume - requires Internet access).
+#' \code{GHCND.station} reads the raw data (slow due to big data volume - requires Internet access).
 
-#' @aliases ghcnd.meta ghcnd.station
-#' @seealso station as.station  
+#' @aliases GHCND.meta GHCND.station
+#' @seealso station as.station ghcnd.meta ghcnd.station
 #' 
 #' @param x a 'stationmeta' object
 #' @param url lURL of the database
@@ -17,7 +17,7 @@
 #' y <- ghcnd.station(subset(m,is=1:10),verbose=TRUE)
 
 #' @export
-ghcnd.meta <- function(url='https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/doc/ghcnd-stations.txt') {
+GHCND.meta <- function(url='https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/doc/ghcnd-stations.txt') {
   # meta <- read.fwf(url,widths = c(11,9,10,7,3,31,4,4,6),
   #                  col.names=c('ID','latitude','longitude','elevation','state','location','GSN-flag','HCN','WMO-ID'))
   ## GHCND
@@ -36,23 +36,36 @@ ghcnd.meta <- function(url='https://www.ncei.noaa.gov/data/global-historical-cli
   # HCN/CRN FLAG 77-79   Character
   # WMO ID       81-85   Character
   # -----------------------------
+  # https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/readme.txt
+  # FIPS country codes https://en.wikipedia.org/wiki/List_of_FIPS_country_codes
+  fips <- readLines('http://efele.net/maps/fips-10/data/fips-414.txt')
+  fips <- fips[grep('country',fips)]
+  fips <- gsub('00_414_414_country____',';',fips)
+  fips <- gsub('__','',fips)
+  fips <- unlist(strsplit(fips,';')); dim(fips) <- c(2,length(fips)/2)
+  ## Get the GHCND metadata
   lines <- readLines(url)
   lines <- lines[nchar(lines)==85]
   id <- substr(lines,1,11)
   lat <- as.numeric(substr(lines,13,20))
   lon <- as.numeric(substr(lines,22,30))
   alt <- as.numeric(substr(lines,32,37))
-  cntr <- substr(lines,39,40)
+  ## Extract country information from the station ID and the embedded FIPS codes
+  cntr <- fips[2, match(substr(id,1,2),fips[1,])]
+  qlty <- substr(lines,73,79)
+  ele <- rep(100.901,length(id))
+  param <- rep('precip+t2m',length(id))
   loc <- substr(lines,42,71)
   wmo.id <- substr(lines,81,85)
   meta <- data.frame(station_id=id,longitude=lon,latitude=lat,altitude=alt,country=cntr,location=loc,
-                     wmo.id=wmo.id,source=rep('GHCND',length(id)),start=rep(NA,length(id)),end=rep(NA,length(id)))
+                     wmo=wmo.id,source=rep('GHCND',length(id)),quality=qlty,element=ele,variable=param,
+                     start=rep(NA,length(id)),end=rep(NA,length(id)))
   class(meta) <- c('stationmeta','data.frame')
   return(meta)
 }
 
 #' @export
-ghcnd.station <- function(x,url='https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/',verbose=FALSE) {
+GHCND.station <- function(x,url='https://www.ncei.noaa.gov/data/global-historical-climatology-network-daily/access/',verbose=FALSE) {
   n <- length(x$station_id)
   if (verbose) print(paste('Read',n,'stations'))
   Precip <- NULL; Tmax <- NULL; Tmin <- NULL; T2m <- NULL
