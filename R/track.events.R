ed7e20f8fa5b8e297f10a0a610cc3b47e447e406
Kajsa Maria Parding <kajsamp@pc5766.pc.met.no>
2022-11-23 10:54:10 +0100

    minor changes of storm track related functions

--- R/track.events.R
+++ R/track.events.R
@@ -372,7 +372,7 @@ Track <- function(x,x0=NULL,it=NULL,is=NULL,dmax=1E6,nmax=124,nmin=3,dmin=1E5,
     ok <- x$n>=nmin | ends
     ok <- ok & (x$distance>=(dmin*1E-3) | ends)
     y <- subset(x,it=ok,verbose=verbose)
-    rnum <- Enumerate(y,verbose=verbose)
+    rnum <- enumerate(y,verbose=verbose)
     rnum[y$trajectory<=n00 & !is.na(rnum)] <- y$trajectory[y$trajectory<=n00 & !is.na(rnum)]
     rnum[y$trajectory>n00 & !is.na(rnum)] <- n00 + rnum[y$trajectory>n00 & !is.na(rnum)]
     y$trajectory <- rnum
@@ -724,7 +724,7 @@ trackstats <- function(x,verbose=FALSE) {
   lats <- y["lat"][[1]]
 
   if(verbose) print("Enumerate")
-  rnum <- Enumerate(y)
+  rnum <- enumerate(y)
   nummax <- max(rnum,na.rm=TRUE)
   rnum[is.na(rnum)] <- nummax+1
   if(verbose) print("trackcount")
@@ -804,9 +804,22 @@ Displacement <- function(x,verbose=FALSE) {
   invisible(dx)
 }
 
-# NOT EXPORTED
-Enumerate <- function(x,param="trajectory",verbose=FALSE) {
-  if(verbose) print("Enumerate")
+#' Enumerate trajectories
+#'
+#' The function enumerates the trajectories ("trajectory") of an event object 
+#' for which the events have been tracked using the function \code{track}. 
+#' \code{enumerate} is called from \code{trackstats} and \code{as.trajectory}
+#' and can also be applied after selecting cyclones using \code{subset} to give 
+#' the trajectories new numbers. 
+#'
+#' @param x an \code{events} object
+#' @param param Name of the column holding the trajectory numbers (default: "trajectory")
+#' @param verbose a boolean; if TRUE print information about progress
+#' @return an \code{events} object
+#'
+#' @export
+enumerate <- function(x,param="trajectory",verbose=FALSE) {
+  if(verbose) print("enumerate")
   stopifnot(inherits(x,"data.frame"))
   num <- x[param][[1]]
   if (identical(unique(num),seq_along(unique(num)))) {
